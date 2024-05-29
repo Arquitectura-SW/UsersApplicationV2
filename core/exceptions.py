7907e69e61551aadc81c8a86a1766d1f38d52eb4
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-05-29 16:48:22 -0500

    Solved bug with errors logs

--- core/exceptions.py
+++ core/exceptions.py
@@ -5,28 +5,32 @@ from fastapi.exceptions import HTTPException
 from core.logs.producer import log
 
 class ClienteDoesNotExist(HTTPException):
+    document: int
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
         self.status_code = status.HTTP_404_NOT_FOUND
         self.message = "Cliente does not exist"
+        self.document = kwargs.get('document')        
     def __str__(self):
         return self.message
         
 async def ClienteDoesNotExistHandler(request: Request, exc: ClienteDoesNotExist):
-    log(document=request.path_params.get('document'), level="ERROR", message=exc.message)
+    log(document=0, level="ERROR", message=exc.message)
     return JSONResponse(status_code=exc.status_code, 
     content={"message": exc.message})
 
 class ClienteAlreadyExists(HTTPException):
+    document: int
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
         self.status_code = status.HTTP_409_CONFLICT
         self.message = "Cliente already exists"
+        self.document = kwargs.get('document')
     def __str__(self):
         return self.message
     
 async def ClienteAlreadyExistsHandler(request: Request, exc: ClienteAlreadyExists):
-    log(document=request.path_params.get('document'), level="ERROR", message=exc.message)
+    log(document=0, level="ERROR", message=exc.message)
     return JSONResponse(status_code=exc.status_code, 
     content={"message": exc.message})
 
@@ -39,7 +43,7 @@ class EmailAlreadyExists(HTTPException):
         return self.message
     
 async def EmailAlreadyExistsHandler(request: Request, exc: EmailAlreadyExists):
-    log(document=request.path_params.get('document'), level="ERROR", message=exc.message)
+    log(document=0, level="ERROR", message=exc.message)
     return JSONResponse(status_code=exc.status_code, 
     content={"message": exc.message})
     
