5f647fa21d1e8ffe0ab0efc20d5c5089ab266847
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-05-29 21:20:44 -0500

    Solved int error

--- core/apps/V1/clientes/services.py
+++ core/apps/V1/clientes/services.py
@@ -28,7 +28,7 @@ def create(cliente : ClienteCreateSchema) -> ClienteSchema:
     return data
 
 def get_by_document(document: int) -> ClienteSchema:
-    data = clientes.find_one({"document": str(document)})
+    data = clientes.find_one({"document": document})
     if not data:
         print("No existe el cliente")
         print(data)
